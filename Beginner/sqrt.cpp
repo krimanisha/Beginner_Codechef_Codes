3c92d0423db0b12cf0b0d6fd2fd17068cea707ec
Manisha Kumari <krimanisha666@gmail.com>
2021-08-24 23:31:28 +0530

    added

---
+++ Beginner/sqrt.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long n;
+        cin>>n;
+        cout<<int(sqrt(n))<<endl;
+    }
+	// your code goes here
+	return 0;
+}
